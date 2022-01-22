5275f80bbe97715762a3530680d9944acef736ad
JohnMarkO <jmolesco@yahoo.com>
2022-01-22 10:14:50 +0800

    New changes in Controller. This is version 1 of the sample codes

---
+++ TechnicalAssignment/Validator/Transaction/InsertTransactionValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalAssignment.Model;
+
+namespace TechnicalAssignment.Validator.Transaction
+{
+    public class InsertTransactionValidator:AbstractValidator<TransactionModel>
+    {
+        public InsertTransactionValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+           
+        }
+    }
+}
